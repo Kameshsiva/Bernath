12b25c1eb8d03844a11c7ce0baef284eff232221
kameshsiva <kameshsivaa@gmail.com>
2021-12-21 22:15:34 +0530

    l

--- src/Gitt/Sample.java
+++ src/Gitt/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Kamesh");
 		System.out.println("Siva");
-
+System.out.println("Abi");
 	
 		System.out.println("Priya");
 		System.out.println("Bernath");
