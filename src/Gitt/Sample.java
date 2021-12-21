f5b1f4c45ba9a391e8d250ab71257e70ea0be989
kameshsiva <kameshsivaa@gmail.com>
2021-12-21 22:05:31 +0530

    Alex to Pandiyan

--- src/Gitt/Sample.java
+++ src/Gitt/Sample.java
@@ -5,6 +5,10 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Kamesh");
 		System.out.println("Siva");
+		
+		
+		System.out.println("Jayan love anushka");
+		System.out.println("Kama ");
 
 	}
 
